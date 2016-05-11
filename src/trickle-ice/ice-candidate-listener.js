2ccefbf413ca28c7be6dfb295538c4131690da02
vogdb <vozhdb@gmail.com>
2016-05-11 17:09:53 +0300

    Close pc on trickle-ice-demo complete

--- src/trickle-ice/ice-candidate-listener.js
+++ src/trickle-ice/ice-candidate-listener.js
@@ -16,6 +16,7 @@ IceCandidateListener.prototype.onIceCandidate = function(event) {
   } else {
     this.emit('complete', this._candidates);
     this._pc.onicecandidate = null;
+    this._pc.close();
   }
 };
 
